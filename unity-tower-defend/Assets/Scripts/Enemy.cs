807e8721bf5a3cc5123bf0734ef1298d5be69a60
Thomas O Fredericks <thomas@fredericks.info>
2025-11-18 11:46:08 -0500

    working a bit

---
+++ unity-tower-defend/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{   
+
+  
+    public GameObject target;
+    // The speed of the enemy
+    public float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. Check if the target exists
+        if (target == null)
+        {
+            // Optional: Find the player if the target hasn't been set
+            // target = GameObject.FindWithTag("Player").transform;
+            return;
+        }
+
+        // 2. Calculate the distance to move this frame
+        // Time.deltaTime ensures the movement is frame-rate independent
+        float step = moveSpeed * Time.deltaTime;
+
+        // 3. Move the current position towards the target position
+        transform.position = Vector2.MoveTowards(
+            transform.position, 
+            target.transform.position, 
+            step
+        );
+    }
+
+}
---
+++ unity-tower-defend/Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float enemySpawnInterval = 1f;
+
+    public GameObject enemyPrefab;
+
+    public GameObject player;
+    float enemySpawnLastTime = 0f;
+
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - enemySpawnLastTime >= enemySpawnInterval)
+        {
+            enemySpawnLastTime = Time.time;
+
+            // Calculate the camera's boundaries in world coordinates
+            float cameraHeight = camera.orthographicSize * 2;
+            float cameraWidth = cameraHeight * camera.aspect;
+
+            float xMin = camera.transform.position.x - (cameraWidth / 2f);
+            float xMax = camera.transform.position.x + (cameraWidth / 2f);
+            float yMin = camera.transform.position.y - (cameraHeight / 2f);
+            float yMax = camera.transform.position.y + (cameraHeight / 2f);
+
+            // Define how far outside the screen the enemy should spawn
+            const float BufferDistance = 0f;
+
+            // Calculate the edges of the spawn zone
+            float spawnXMin = xMin - BufferDistance;
+            float spawnXMax = xMax + BufferDistance;
+            float spawnYMin = yMin - BufferDistance;
+            float spawnYMax = yMax + BufferDistance;
+
+            int side = Random.Range(0, 4);
+            Vector3 spawnPosition;
+
+            if (side == 0) // Left side
+            {
+                // Fix X to the buffer edge, randomize Y within the camera's vertical range
+                float randomY = Random.Range(yMin, yMax);
+                spawnPosition = new Vector3(spawnXMin, randomY, 0f);
+            }
+            else if (side == 1) // Right side
+            {
+                // Fix X to the buffer edge, randomize Y
+                float randomY = Random.Range(yMin, yMax);
+                spawnPosition = new Vector3(spawnXMax, randomY, 0f);
+            }
+            else if (side == 2) // Top side
+            {
+                // Fix Y to the buffer edge, randomize X
+                float randomX = Random.Range(xMin, xMax);
+                spawnPosition = new Vector3(randomX, spawnYMax, 0f);
+            }
+            else // Bottom side (side == 3)
+            {
+                // Fix Y to the buffer edge, randomize X
+                float randomX = Random.Range(xMin, xMax);
+                spawnPosition = new Vector3(randomX, spawnYMin, 0f);
+            }
+
+           GameObject instance = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+           Enemy enemyScript = instance.GetComponent<Enemy>();
+            enemyScript.target = player;
+
+        }
+
+    }
+}
